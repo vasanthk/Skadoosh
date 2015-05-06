79c5630b87173242e1e0d0ae89e5f8195a0990d9
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-05-05 17:26:44 -0700

    Firechat with Firebase -- Working with anonymous login! TODO: Needs better styling!

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+// viewed at http://localhost:8080
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.use('/js', express.static(path.join(__dirname, '/js')));
+app.use('/css', express.static(path.join(__dirname, '/css')));
+app.use('/fonts', express.static(path.join(__dirname, '/fonts')));
+
+app.listen(4000);
